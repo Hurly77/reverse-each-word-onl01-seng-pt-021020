643bf36f0e593ecc961852164b54c3bf868098cd
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-18 06:47:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
-reverse_string = string.split("")
-empty_array = []
-reverse_string.each do |string| empty_array << string.reverse
-end
-return  empty_array.join("")
-  end
+  original_array = string.split(" ")
+   return_array = []
+   original_array.each do|string|
+     return_array << string.reverse
+   end
+   return_array.join(" ")
+ end
